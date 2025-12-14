490c51fd1ead45ecb5f0220fa3f56407a74e953b
Yonatan Abergel <yonatanab1@gmail.com>
2025-12-14 21:50:23 +0200

    fix first build working
    removed some useless files which contains errors that appear upon build

--- Assets/OverallAssets/version verifier/VersionUpdater.cs
+++ Assets/OverallAssets/version verifier/VersionUpdater.cs
@@ -1,6 +1,9 @@
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 
+#if UNITY_EDITOR
 [InitializeOnLoad]
 public static class VersionUpdater
 {
@@ -26,3 +29,4 @@ public static class VersionUpdater
         }
     }
 }
+#endif
